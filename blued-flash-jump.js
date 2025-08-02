7ca2ea5fb4edb8d9ffe861da6624164411fac9c3
coll1 <68933908+coll1@users.noreply.github.com>
2025-08-02 23:15:27 +0800

    blued-flash-jump.js

--- blued-flash-jump.js
+++ blued-flash-jump.js
@@ -1,2 +1,16 @@
-$notification.post("测试成功", "说明脚本已触发", $request.url);
+let url = $request.url;
+let headers = $request.headers;
+
+function notify(title = "", subtitle = "", content = "", open_url) {
+    let opts = {};
+    if (open_url) opts["open-url"] = open_url;
+    if (Object.keys(opts).length === 0) {
+        $notification.post(title, subtitle, content);
+    } else {
+        $notification.post(title, subtitle, content, opts);
+    }
+}
+
+notify("📸 Blued 闪照已捕获", "点击跳转到浏览器打开原图", url, url);
+console.log("捕获闪照链接: " + url);
 $done({});
